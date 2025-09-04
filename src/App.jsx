95807de6dbccbd314c88bf575ef535f11cd3bac6
Mathu <z5419643@ad.unsw.edu.au>
2025-09-04 15:40:06 +1000

    Implemented header page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import bg from "./assets/BG.png";
+import Search from "./components/search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div>
-      <main>
+      <main style={{ backgroundImage: `url(${bg})` }}>
         <div className="pattern" />
         <div className="wrapper">
           <header>
+            <img src="src/assets/hero-img.png" alt="Hero" />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without Hassle
             </h1>
           </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
       </main>
     </div>
