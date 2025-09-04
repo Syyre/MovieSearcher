ca97d9361915d8fc0a3f826a9dda9e7f157d5e21
Mathu <z5419643@ad.unsw.edu.au>
2025-09-04 14:43:49 +1000

    Added tailwind and abit of CSS

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <div className="pattern" />
+        <div className="wrapper">
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without Hassle
+            </h1>
+          </header>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default App;
